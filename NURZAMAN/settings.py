11476ea4367f523941234e578de64797841185c6
Marcus <maaaarcxsmoraes@example.com>
2025-12-16 10:22:02 +0600

    mistakes corrected

--- NURZAMAN/settings.py
+++ NURZAMAN/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pm4@v!ye1(0@48!nc&3t89m&ide46a(ux6&9c9$fnsu5b2@dpt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -130,7 +130,11 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
